488d09577d74a5833368092e82c1c2a85272c4d6
ReevesAg <reeves@antigravitygroup.ng>
2023-06-17 21:08:21 +0100

    token endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from .views import SignUp, Login, CreateStockUpSchedule, DeleteStockUpSchedule, UpdateStockUpSchedule
+from .views import (
+    SignUp, Login, 
+    CreateStockUpSchedule, DeleteStockUpSchedule, 
+    UpdateStockUpSchedule, MyTokenObtainPairView
+)    
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
  
 urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('sign_up/', SignUp .as_view(), name='signup'),
     path('login/', Login.as_view(), name='login'),
     path('create_stock_up/', CreateStockUpSchedule.as_view(), name='create_stock_up'),
