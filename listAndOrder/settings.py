eaf853140943872e869e6a7b31289956aca83b59
ReevesAg <reeves@antigravitygroup.ng>
2023-06-24 22:15:40 +0100

    online admin tweaks

--- listAndOrder/settings.py
+++ listAndOrder/settings.py
@@ -13,10 +13,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import dj_database_url
+from os import path
+import sys
 
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+PROJECT_ROOT = path.normpath(path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,6 +139,7 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
