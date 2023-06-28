5d790a7752954941f4fa28c2a68ab84d9814131b
ReevesAg <reeves@antigravitygroup.ng>
2023-06-28 22:04:06 +0100

    statis directory

--- listAndOrder/settings.py
+++ listAndOrder/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
